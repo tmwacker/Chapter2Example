b2d695f6ef0a8482e7795ca4e9a3cdd72350b3d0
tmwacker <tmw24859@email.vccs.edu>
2024-09-09 03:46:53 +0000

    Example

--- main.cpp
+++ main.cpp
@@ -1,3 +1,22 @@
-#include <iostream>
+//This program calculates and displays the several values
+// and various percentages for those values
 
-int main() { std::cout << "Hello World!\n"; }
+
+#include <iostream>
+using namespace std;
+int main()
+{
+double salary = 6000;
+double percent1 = 5, percent2 = 7, percent3 = 10;
+double contribution1, contribution2, contribution3;
+contribution1 = salary * percent1 / 100;
+contribution2 = salary * percent2 / 100;
+contribution3 = salary * percent3 / 100;
+cout << "Amount of contribution at " << percent1 << "% is $" <<
+contribution1 << endl;
+cout << "Amount of contribution at " << percent2 << "% is $" <<
+contribution2 << endl;
+cout << "Amount of contribution at " << percent3 << "% is $" <<
+contribution3 << endl;
+return 0;
+}
